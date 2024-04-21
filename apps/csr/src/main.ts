1cdd5c1c2e607da36d0801ac647360d86bce6aac
SoJuSo <asdfg7123@gmail.com>
2024-04-21 22:57:39 +0900

    ✨feat : innerHTML 제거 마이그레이션 중 WIP 기록
    `@repo/ui/header`에 있는 Header 생성 컴포넌트를 사용하니 문제가 발생했다.
    해당 컴포넌트는 HTML을 문자열로 파싱해서 전달해주기에 appendChild가 동작하지 않는 문제가 발생한다.
    따라서 해당 문제점을 DOM 요소로 변환하는 과정이 필요해서 createElementFromHTML를 제작하였다.

--- apps/csr/src/main.ts
+++ apps/csr/src/main.ts
@@ -3,20 +3,36 @@ import "./style.css";
 import typescriptLogo from "/typescript.svg";
 import { Counter } from "@repo/ui/counter";
 import { setupCounter } from "@repo/ui/setup-counter";
+const $ = document;
+const $target = $.querySelector<HTMLDivElement>("#app");
 
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    ${Header({ title: "csr" })}
-    <div class="card">
-      ${Counter()}
+const createElementFromHTML = (htmlString: string): ChildNode => {
+  const div = document.createElement("div");
+  div.innerHTML = htmlString.trim();
+  return div.firstChild as ChildNode;
+};
+
+if ($target) {
+  $target.innerHTML = `
+    <div>
+      <a href="https://vitejs.dev" target="_blank">
+        <img src="/vite.svg" class="logo" alt="Vite logo" />
+      </a>
+      <a href="https://www.typescriptlang.org/" target="_blank">
+        <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
+      </a>
+      ${Header({ title: "csr" })}
+      <div class="card">
+        ${Counter()}
+      </div>
     </div>
-  </div>
-`;
+    `;
 
-setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
+  // const $Header = $.createElement("div");
+  const $Header = createElementFromHTML(Header({ title: "csr" }));
+  $target.appendChild($Header);
+
+  setupCounter($.querySelector<HTMLButtonElement>("#counter")!);
+} else {
+  alert(`에러 발생!`);
+}
