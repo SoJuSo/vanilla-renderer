a88816d62f72f6b8e3301c63a220c20a07be7c22
SoJuSo <asdfg7123@gmail.com>
2024-06-05 23:54:47 +0900

    ✨feat : 동적으로 색상을 추가할 수 있도록 기능 구현

--- packages/ui/components/tree.ts
+++ packages/ui/components/tree.ts
@@ -3,8 +3,15 @@ import styles from "../styles/treeButton.module.css";
 interface Tree {
   treeName: string;
   treeId: string;
+  color?: string;
 }
 
-export function Tree({ treeName, treeId }: Tree) {
-  return `<button class=${styles.treeButton} id="tree${treeId}" type="button">${treeName}</button>`;
+export function Tree({ treeName, treeId, color }: Tree) {
+  const buttonStyle = {
+    color: color || "yellow",
+  };
+
+  return `<button style=${Object.entries(buttonStyle)
+    .map(([key, value]) => `${key}:${value}`)
+    .join(";")} class=${styles.treeButton} id="tree${treeId}" type="button">${treeName}</button>`;
 }
